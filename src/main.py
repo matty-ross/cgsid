264704902d4abdfeb49b1d30d2479903dbf0617a
Matty <54020396+matty-ross@users.noreply.github.com>
2024-02-14 18:49:27 +0100

    Small refactor.

--- src/main.py
+++ src/main.py
@@ -2,13 +2,12 @@ import cgsid
 
 
 def main() -> None:
-    action = input('[c]ompress [u]ncompress ? ')
+    action = input("[c]ompress [u]ncompress ? ")
     
     if action == 'c':
         string = input("String: ")
         id = cgsid.compress(string)
         print(f'{id :016X}')
-    
     elif action == 'u':
         id = int(input("ID: "), 16)
         string = cgsid.uncompress(id)
